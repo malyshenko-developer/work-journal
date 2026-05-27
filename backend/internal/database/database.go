9ddac97156414efdbf6e014cf28428406017a7bb
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2026-05-27 16:33:39 +0800

    backend: connect to database and run migrations on startup

---
+++ backend/internal/database/database.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"))
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	DB = db
+	log.Println("Connected to database")
+}
+
+func RunMigrations() {
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+
+	m, err := migrate.New("file://migrations", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to init migrations: %v", err)
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+
+	log.Println("Migrations applied")
+}
--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"work-journal/backend/internal/database"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables.")
+	}
+
+	database.Connect()
+	database.RunMigrations()
+
 	r := gin.Default()
 
 	r.GET("/health", func(c *gin.Context) {
